b6d60eb1fcf650f41861bd5f62f55b5897079659
EasyApp_Jim <jim@easyapp.com.tw>
2016-06-03 16:49:36 +0800

    init

---
+++ app/src/main/java/com/easyapp/easydb/TestItem.java
@@ -0,0 +1,11 @@
+package com.easyapp.easydb;
+
+/**
+ * 測試用db
+ */
+public class TestItem extends Model {
+    @Override
+    public String getListKey() {
+        return "LISTKEY";
+    }
+}
---
+++ lib/src/main/java/com/easyapp/easydb/Model.java
@@ -0,0 +1,114 @@
+package com.easyapp.easydb;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基本的database model
+ */
+public abstract class Model {
+
+    public abstract String getListKey();
+
+
+    /**
+     * 唯一值不可操作
+     */
+    @SerializedName("uniqueId")
+    private String uniqueId;
+
+    private String getUniqueId() {
+        return uniqueId;
+    }
+
+    private void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+
+    final public String getGson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+
+    final public void removeAll(Context context) {
+        EasyDB preferenceDB = new EasyDB(context);
+        preferenceDB.putList(getListKey(), new ArrayList<String>());
+    }
+
+    /**
+     * 取回所有項目
+     *
+     * @param context
+     * @return
+     */
+    final public List getAll(Context context) {
+        EasyDB preferenceDB = new EasyDB(context);
+        ArrayList<String> arrayList = preferenceDB.getList(getListKey());
+        List shopCarItems = new ArrayList<>();
+        Gson gson = new Gson();
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            shopCarItems.add(gson.fromJson(arrayList.get(i), getClass()));
+        }
+        return shopCarItems;
+    }
+
+
+    /**
+     * 從list 刪除項目
+     *
+     * @param context
+     */
+    public void delete(Context context) {
+        EasyDB preferenceDB = new EasyDB(context);
+        ArrayList<String> arrayList = preferenceDB.getList(getListKey());
+        int position = arrayList.indexOf(this.getGson());
+        if (position != -1) {
+            arrayList.remove(position);
+        }
+        preferenceDB.putList(getListKey(), arrayList);
+    }
+
+    /**
+     * 儲存項目到list
+     *
+     * @param context
+     */
+    final public void save(Context context) {
+        EasyDB preferenceDB = new EasyDB(context);
+        ArrayList<String> arrayList = preferenceDB.getList(getListKey());
+        this.setUniqueId(preferenceDB.RandUUID());
+        arrayList.add(this.getGson());
+        preferenceDB.putList(getListKey(), arrayList);
+    }
+
+    /**
+     * 更新單一項目儲存到list
+     *
+     * @param context
+     */
+    final public void update(Context context) {
+        EasyDB preferenceDB = new EasyDB(context);
+        ArrayList<String> arrayList = preferenceDB.getList(getListKey());
+        int position = arrayList.indexOf(this.getGson());
+        Gson gson = new Gson();
+        for (int i = 0; i < arrayList.size(); i++) {
+            Model item = gson.fromJson(arrayList.get(i), this.getClass());
+            if (item.getUniqueId().equals(this.getUniqueId())) {
+                position = i;
+                break;
+            }
+        }
+        if (position != -1) {
+            arrayList.set(position, this.getGson());
+        }
+        preferenceDB.putList(getListKey(), arrayList);
+    }
+}
